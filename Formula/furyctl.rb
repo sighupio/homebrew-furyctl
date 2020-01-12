ad043d43c3d210a8dac40af57fede2d740cf8553
Luca Zecca <l.zecca78@gmail.com>
2020-01-12 08:19:40 +0100

    added conflicts_with

--- Formula/furyctl.rb
+++ Formula/furyctl.rb
@@ -6,6 +6,11 @@ class Furyctl < Formula
   url "https://github.com/sighupio/furyctl/releases/download/v#{FURYCTL_VERSION}/furyctl-darwin-amd64"
   sha256 'b7a02c1ef007df54a1266f3675e71742afecb4ab307d46cae945ca11917862c9'
 
+  bottle :unneeded
+ 
+  #if was previously installed manually in another path
+  conflicts_with "furyctl", :because => "furyctl binary is owned by this setup"
+  
     def install
       bin.install 'furyctl'
     end
