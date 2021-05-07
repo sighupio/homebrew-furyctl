5c73946aab2e6914760366090a8cf4c27a99bb52
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-07 14:57:46 +0300

    Brew formula update for furyctl version v0.5.0-rc2

--- Formula/furyctl.rb
+++ Formula/furyctl.rb
@@ -2,16 +2,16 @@
 class Furyctl < Formula
   desc "Furyctl  binary"
   homepage "https://gihub.com/sighupio/furyctl"
-  version "0.4.0"
+  version "0.5.0-rc2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sighupio/furyctl/releases/download/v0.4.0/furyctl-darwin-amd64.tar.gz"
-    sha256 "4afcbc321b2beaf941205403c3be73bfb629cd8e8148c487b7d586d1db1fa7e5"
+    url "https://github.com/sighupio/furyctl/releases/download/v0.5.0-rc2/furyctl-darwin-amd64.tar.gz"
+    sha256 "f4bd14cf1ba5811fd11d73ccf977cbd61cb2e56d1c99e58308ce432091a940a9"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/sighupio/furyctl/releases/download/v0.4.0/furyctl-linux-amd64.tar.gz"
-    sha256 "9d2b1c6061a7649e5864a61db03311846b7caa5df5524cbb7b943c728b07b243"
+    url "https://github.com/sighupio/furyctl/releases/download/v0.5.0-rc2/furyctl-linux-amd64.tar.gz"
+    sha256 "c7c787716d60e71786505831fa2eb8a4d7853aa12b80e55e37f2c8d436384a37"
   end
 
   conflicts_with "furyctl"
